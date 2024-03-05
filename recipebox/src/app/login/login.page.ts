dd9a09bdc379af5b9f0e98c1ec5e4ca49f71595f
Lilian <lswann@vols.utk.edu>
2024-03-05 17:26:34 -0500

    adding login page

--- recipebox/src/app/login/login.page.ts
+++ recipebox/src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  login = {
+    username: '',
+    password: ''
+  }
+
+  constructor(private http: HttpClient, private _router: Router) { }
 
   ngOnInit() {
+  }
+  
+  saveRecipe() {
+
+    this.http.post('http://127.0.0.1:5000/login/', this.login)
+        .subscribe({
+          next: (response) => {
+            console.log('POST Response:', response);
+          },
+
+          error: (error) => {
+            console.error("POST error", error);
+          },
+
+          complete: () => {},
+      });
+    }
+
+  goHome(){
+    this._router.navigate(['/home'])
   }
 
 }
